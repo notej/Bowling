033fbca42da7409df8bfc976bd57da1269bf5516
Jeton Veseli <35177587+notej@users.noreply.github.com>
2026-09-09 23:28:12 +0200

    Remove static file serving from server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-const publicPath = path.join(__dirname, 'public');
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
-});
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const WebSocket = require('ws');
